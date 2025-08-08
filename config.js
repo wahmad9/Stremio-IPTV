bd2200fc9316027c000afc0d4414213f517bac5a
wahmad9 <97993881+wahmad9@users.noreply.github.com>
2025-08-08 19:51:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-var env = process.env.NODE_ENV ? 'beamup':'local';
+var env = process.env.NODE_ENV === 'production' ? 'production' : 'local';
 
 var config = {}
 
 switch (env) {
-    case 'beamup':
-		config.port = process.env.PORT
-        config.local = "https://perceptive-healing.up.railway.app/manifest.json"
+    case 'production': // for Railway
+        config.port = process.env.PORT;
+        config.local = "https://perceptive-healing.up.railway.app/manifest.json";
         break;
 
     case 'local':
-		config.port = 63355
-        config.local = "http://127.0.0.1:" + config.port;
+        config.port = 63355;
+        config.local = "http://127.0.0.1:" + config.port + "/manifest.json";
         break;
 }
 
