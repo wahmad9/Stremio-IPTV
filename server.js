ce517b1ce0b9692593f459a325c6a3b806d3f1f3
wahmad9 <97993881+wahmad9@users.noreply.github.com>
2025-08-08 19:48:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
 const config = require('./config.js');
 
 // create local server
-app.listen((config.port), function () {
+app.listen(config.port, function () {
     console.log(`Addon active on port ${config.port}`);
-    console.log(`HTTP addon accessible at: ${config.local}/configure`);
+    console.log(`HTTP addon accessible at: ${config.local.replace('/manifest.json','')}/configure`);
+    console.log(`Manifest URL: ${config.local}`);
 });
 
-if(process.env.NODE_ENV){
-    publishToCentral(`${config.local}/manifest.json`)
+if (process.env.NODE_ENV) {
+    publishToCentral(config.local)
 }
